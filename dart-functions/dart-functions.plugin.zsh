8cc8413a29aaa302e5755a19a34c6248632ff566
Elijah Luckey <luckeyelijah112@gmail.com>
2022-10-25 11:01:40 -0400

    improvements on reading commands and variables

--- dart-functions/dart-functions.plugin.zsh
+++ dart-functions/dart-functions.plugin.zsh
@@ -1,47 +1,37 @@
+#!/bin/bash
 autoload -Uz is-at-least
 
-GREEN='\033[0;32m'
-RED="\033[0;31m"
-NC='\033[0m' # No Color
+local RED="\033[0;31m"
+local BOLD='\033[1m'
+local NC='\033[0m' # No Color
 
-function _has_dart_installed() {
-    if [ -f $(which dart) ]; then
-        return 0
-    else
-        echo "${RED}'dart' is not available on the current \$PATH.${NC} 👎"
-        return 1
+_has_dart_installed() {
+    if [ -x "$(command -v dart)" ]; then; else
+        echo "${RED}'dart' is not available on the current \$PATH.${NC}" >&2
     fi
 }
 
-function _has_dart_fn_set() {
-    if [ -d $DART_FUNCTIONS ]; then
-        return 0
-    else
-        echo "${RED}\$DART_FUNCTIONS is not set correctly.${NC} 👎"
-        echo "Add ${RED}DART_FUNCTIONS=\"\$HOME/path/to/dart/functions\"${NC} to your ~/.zshrc"
-        return 1
+_has_dart_fn_set() {
+    if [ -v DART_FUNCTIONS ]; then; else
+        echo "${RED}${BOLD}DART_FUNCTIONS${NC}${RED} is not set correctly.${NC}" >&2
     fi
 }
 
-function _dart_fn_assert() {
-    if _has_dart_installed; then
-        if _has_dart_fn_set; then
-            return 0
-        else
-            return 1
-        fi
-    fi
-    return 1
-}
 
 function dfn() {
-    _dart_fn_assert
-    matcher=$1
-    dart_file="$DART_FUNCTIONS/$matcher.dart"
+    # check if dart is installed and has variable set
+    _has_dart_installed; _has_dart_fn_set;
+
+    local matcher=$1
+
+    # may not work for file names w/ white space
+    local dart_file="$DART_FUNCTIONS/$matcher.dart"
+
     if [ -f $dart_file ]; then
+        # send the remaining args to the dart script
         shift
         dart run $dart_file $@
     else
-        echo "${RED}$dart_file${NC} does not exist."
+        echo "${RED}$dart_file${NC} does not exist." >&2
     fi
 }
